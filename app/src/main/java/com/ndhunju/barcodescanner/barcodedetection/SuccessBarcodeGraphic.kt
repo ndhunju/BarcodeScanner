2c80f4ca70756faf2a58d956515841c53846c3f1
ndhunju <dhunjunikesh@gmail.com>
2024-03-14 15:41:01 -0400

    First commit of new android project to modularize part of code for barcode scanning.

---
+++ app/src/main/java/com/ndhunju/barcodescanner/barcodedetection/SuccessBarcodeGraphic.kt
@@ -0,0 +1,19 @@
+package com.ndhunju.barcodescanner.barcodedetection
+
+import android.graphics.Paint
+import androidx.core.graphics.toColorInt
+import com.ndhunju.barcode.R
+import com.ndhunju.barcode.camera.GraphicOverlay
+import com.ndhunju.barcodescanner.barcodedetection.BarcodeGraphicBase
+
+class SuccessBarcodeGraphic(overlay: GraphicOverlay) : BarcodeGraphicBase(overlay) {
+
+    override var boxBorderPaint: Paint = Paint().apply {
+        color = "#6EDA78".toColorInt()
+        style = Paint.Style.STROKE
+        strokeWidth = context.resources.getDimensionPixelOffset(
+            R.dimen.barcode_reticle_stroke_width
+        ).toFloat()
+    }
+
+}
