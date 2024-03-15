98bfbba5d49151990732fede93e1c66a51d3a110
ndhunju <dhunjunikesh@gmail.com>
2024-03-15 16:15:13 -0400

    Possible fix for jitpack.io error Gradle 'publishToMavenLocal' task not found.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ buildscript {
     }
 }
 plugins {
+    `maven-publish`
     id("com.android.library") version "8.2.1" apply false
     id("com.android.application") version "8.2.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
